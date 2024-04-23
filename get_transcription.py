5672605c6fa5100a2335ac27cdd54bcc41b6d6cf
unknown <jonbidezabal@gmail.com>
2024-04-23 23:10:16 +0200

    first commit

---
+++ get_transcription.py
@@ -0,0 +1,13 @@
+import speech_recognition as transcriptor
+
+def transcribe_audio(audio_file):
+    try:
+        recognizer = transcriptor.Recognizer()
+        with transcriptor.AudioFile(audio_file) as source:
+            audio_data = recognizer.record(source)
+        text = recognizer.recognize_google(audio_data, language="es-ES")  
+        return text
+    except transcriptor.UnknownValueError:
+        return "No se pudo entender el audio"
+    except transcriptor.RequestError as e:
+        return f"Fallo en la transcripción, {str(e)}"
